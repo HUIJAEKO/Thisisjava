fa6e39d6a4ab86ff779cb498a5a99d778376e8b5
HUIJAEKO <143581510+HUIJAEKO@users.noreply.github.com>
2023-11-27 21:29:47 +0900

    Add files via upload

---
+++ chapter2/ch02/sec02/TextBlockExample.java
@@ -0,0 +1,17 @@
+package ch02.sec02;
+
+public class TextBlockExample {
+
+	public static void main(String[] args) {
+		
+		String str1 = """
+				{
+				나는 "자바"를\
+				좋아한다
+				}
+				""";
+		
+		System.out.println(str1);
+	}
+
+}
