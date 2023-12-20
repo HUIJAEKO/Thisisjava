32d4663976914d6c546601ec3c6cf30ee32d39cd
HUIJAEKO <143581510+HUIJAEKO@users.noreply.github.com>
2023-12-20 10:57:21 +0900

    자바 1~6

---
+++ ch02변수/sec02/ScannerExample.java
@@ -0,0 +1,23 @@
+package ch02변수.sec02;
+
+import java.util.Scanner;
+
+public class ScannerExample {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("x값을 입력하세요:");		
+		String str = scanner.nextLine();
+		int x = Integer.parseInt(str);
+		
+		System.out.println("x의 값은" + x + "입니다");
+		
+		
+		int y = 365;
+		int z = 100;
+		System.out.println((double)y/z);
+	}
+
+}
---
+++ ch06연습문제/Accontex.java
@@ -0,0 +1,21 @@
+package ch06연습문제;
+
+public class Accontex {
+
+	public static void main(String[] args) {
+		Account account = new Account();
+		
+		account.setBalance(10000);
+		System.out.println("현재잔고:"+account.getBalance());
+		
+		account.setBalance(20000);
+		System.out.println("현재잔고:"+account.getBalance());
+		
+		account.setBalance(-10000);
+		System.out.println("현재잔고:"+account.getBalance());
+		
+		account.setBalance(5000);
+		System.out.println("현재잔고:"+account.getBalance());
+	}
+
+}
